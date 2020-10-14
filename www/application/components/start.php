d344aa639b0e80816e049f76acbe48f719815bf9
Ivan Voitovych <voitovych.ivan.v@gmail.com>
2020-10-14 16:50:24 +0300

    build config

--- www/application/components/start.php
+++ www/application/components/start.php
@@ -7,13 +7,13 @@ include 'routes.php';
 $develop = true;
 $renderReturn = true;
 $upFolder = DIRECTORY_SEPARATOR . '..';
-$buildRoot = __DIR__ . $upFolder . $upFolder . DIRECTORY_SEPARATOR;
-
+$buildRoot = __DIR__ . $upFolder . DIRECTORY_SEPARATOR;
+$publicBuildRoot = __DIR__ . $upFolder . $upFolder . DIRECTORY_SEPARATOR;
 Viewi\App::init(
     [
         'SOURCE_DIR' =>  __DIR__,
         'SERVER_BUILD_DIR' => $buildRoot . 'build',
-        'PUBLIC_BUILD_DIR' => $buildRoot . 'public' . DIRECTORY_SEPARATOR . 'build',
+        'PUBLIC_BUILD_DIR' => $publicBuildRoot . 'public' . DIRECTORY_SEPARATOR . 'build',
         'DEV_MODE' => $develop,
         'RETURN_OUTPUT' => $renderReturn
     ]
