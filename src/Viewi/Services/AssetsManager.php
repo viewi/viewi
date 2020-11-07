f52859626132d9cf0c87316bfda1e09e79392102
Ivan Voitovych <voitovych.ivan.v@gmail.com>
2020-11-07 13:10:49 +0200

    AssetsManager: build version

--- src/Viewi/Services/AssetsManager.php
+++ src/Viewi/Services/AssetsManager.php
@@ -12,15 +12,17 @@ class AssetsManager
         $path = App::$config[PageEngine::PUBLIC_URL_PATH] ?? '/';
         $combine = App::$config[PageEngine::COMBINE_JS] ?? false;
         $minify = App::$config[PageEngine::MINIFY] ?? false;
+        $dev = App::$config[PageEngine::DEV_MODE];
+        $version = $dev ? '' : '?v=' . date('ymdHis');
         $async = $combine ? 'async' : '';
         $scripts = $minify ?
-            "<script $async src=\"$path/app.min.js\"></script>"
-            : "<script $async src=\"$path/app.js\"></script>";
+            "<script $async src=\"$path/app.min.js$version\"></script>"
+            : "<script $async src=\"$path/app.js$version\"></script>";
         if (!$combine) {
             $scripts =
                 ($minify ?
-                    "<script src=\"$path/bundle.min.js\"></script>"
-                    : "<script src=\"$path/bundle.js\"></script>") .
+                    "<script src=\"$path/bundle.min.js$version\"></script>"
+                    : "<script src=\"$path/bundle.js$version\"></script>") .
                 $scripts;
         }
         return $scripts;
