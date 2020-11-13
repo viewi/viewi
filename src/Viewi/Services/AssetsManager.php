d0d0b66d375d15d7f6d2077edba5227443b7f121
Ivan Voitovych <voitovych.ivan.v@gmail.com>
2020-11-13 15:57:07 +0200

    CssBundle component: combine, minify, inline support

--- src/Viewi/Services/AssetsManager.php
+++ src/Viewi/Services/AssetsManager.php
@@ -33,15 +33,53 @@ class AssetsManager
     {
         $html = '';
         $rootDir = App::$config[PageEngine::PUBLIC_ROOT_DIR];
+        $buildDir = App::$config['PUBLIC_BUILD_DIR'];
+        $combined = '';
+        $dev = App::$config[PageEngine::DEV_MODE];
+        $version = $dev ? '' : '?v=' . date('ymdHis');
+        $componentVersion = $bundle->__version();
+        $minifyService = new MinifyService();
         if ($bundle->link) {
-            $html .= $bundle->inline ?
-                '<style>' . file_get_contents($rootDir . $bundle->link) . '</style>' :
-                "<link rel=\"stylesheet\" href=\"{$bundle->link}\">";
+            $cssName = $bundle->link;
+            $cssContent = ($bundle->combine || $bundle->minify) ? file_get_contents($rootDir . $cssName) : '';
+            if ($bundle->minify) {
+                $cssContent = $minifyService->minifyCss($cssContent);
+                $cssName = $buildDir . '/' . basename($bundle->link, '.css') . '.min.css';
+                $newFileName =  $rootDir . $cssName;
+                file_put_contents($newFileName, $cssContent);
+            }
+            if ($bundle->combine) {
+                $combined .= $cssContent;
+            } else {
+                $html .= $bundle->inline ?
+                    '<style>' . file_get_contents($rootDir . $cssName) . '</style>' :
+                    "<link rel=\"stylesheet\" href=\"{$cssName}$version\">";
+            }
         }
         foreach ($bundle->links as $link) {
+            $cssName = $link;
+            $cssContent = ($bundle->combine || $bundle->minify) ? file_get_contents($rootDir . $cssName) : '';
+            if ($bundle->minify) {
+                $cssContent = $minifyService->minifyCss($cssContent);
+                $cssName = $buildDir . '/' . basename($cssName, '.css') . '.min.css';
+                $newFileName =  $rootDir . $cssName;
+                file_put_contents($newFileName, $cssContent);
+            }
+            if ($bundle->combine) {
+                $combined .= $cssContent . ($bundle->minify ? '' : PHP_EOL);
+            } else {
+                $html .= $bundle->inline ?
+                    '<style>' . file_get_contents($rootDir . $cssName) . '</style>' :
+                    "<link rel=\"stylesheet\" href=\"{$cssName}$version\">";
+            }
+        }
+        if ($bundle->combine) {
+            $newName = $buildDir . '/' . crc32($componentVersion) . '.css';
+            $newFileName =  $rootDir . $newName;
+            file_put_contents($newFileName, $combined);
             $html .= $bundle->inline ?
-                '<style>' . file_get_contents($rootDir . $link) . '</style>' :
-                "<link rel=\"stylesheet\" href=\"{$link}\">";
+                '<style>' . $combined . '</style>' :
+                "<link rel=\"stylesheet\" href=\"{$newName}$version\">";
         }
         return $html;
     }
---
+++ src/Viewi/Services/MinifyService.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Viewi\Services;
+
+class MinifyService
+{
+    private string $css = '';
+    private string $mini = '';
+    private int $count;
+    private int $i = 0;
+    private string $previousToken;
+    private bool $previousSafe;
+    private bool $previousWhiteSpace;
+    private array $safeTokensAfter;
+    private array $safeTokensBefore;
+
+    function __construct()
+    {
+        $this->safeTokensAfter = array_flip(str_split('{},;:)'));
+        $this->safeTokensBefore = array_flip(str_split('{},;:('));
+    }
+
+    public function minifyCss(string $cssText): string
+    {
+        $this->css = $cssText;
+        $this->count = strlen($this->css);
+        $this->mini = '';
+        $this->i = 0;
+        $this->previousToken = ' ';
+        $this->previousSafe = true;
+        $this->previousWhiteSpace = true;
+        // print_r($this->safeTokensBefore);
+        while ($this->i < $this->count) {
+            $keyword = $this->css[$this->i];
+            switch ($keyword) {
+                case '/': {
+                        if ($this->i + 1 < $this->count && $this->css[$this->i + 1] === '*') {
+                            // comment begin
+                            $this->readComment();
+                            break;
+                        }
+                        $this->mini .= $keyword;
+                        break;
+                    }
+                case '"': {
+                        $this->mini .= $this->readString('"');
+                        break;
+                    }
+                case '\'': {
+                        $this->mini .= $this->readString('\'');
+                        break;
+                    }
+                default: {
+                        if (
+                            $this->previousToken === ';'
+                            && $keyword === '}'
+                        ) { // ;}
+                            $this->previousToken = $keyword;
+                            $this->mini[strlen($this->mini) - 1] = $keyword;
+                            break;
+                        }
+                        $currentWhiteSpace = ctype_space($keyword);
+                        $nextSafe = false;
+                        if ($currentWhiteSpace) {
+                            $keyword = ' ';
+                            $nextSafe = $this->i >= $this->count || isset($this->safeTokensAfter[$this->css[$this->i + 1]]);
+                        } else {
+                            $this->previousToken = $keyword;
+                        }
+                        $currentSafe = isset($this->safeTokensBefore[$keyword]);
+                        if (
+                            !$currentWhiteSpace // not white space
+                            || (!$this->previousWhiteSpace // white space after white space
+                                && !$this->previousSafe // white space after not safe
+                                && !$nextSafe) // white space before not safe
+                        ) {
+                            $this->mini .= $keyword;
+                        }
+
+                        $this->previousSafe = $currentSafe;
+                        $this->previousWhiteSpace = $currentWhiteSpace;
+                    }
+            }
+
+            $this->i++;
+        }
+        // print_r($this->mini);
+        return $this->mini;
+    }
+
+    private function readComment()
+    {
+        $this->i += 2;
+        while ($this->i < $this->count) {
+            if (
+                $this->css[$this->i] === '*' &&
+                $this->i + 1 < $this->count &&
+                $this->css[$this->i + 1] === '/'
+            ) {
+                $this->i += 2;
+                return;
+            }
+            $this->i++;
+        }
+    }
+
+    private function readString(string $quote)
+    {
+        $string = $quote;
+        $this->i++;
+        while ($this->i < $this->count) {
+            if (
+                $this->css[$this->i] === $quote &&
+                $this->css[$this->i - 1] !== '\\'
+            ) {
+                $string .= $quote;
+                return $string;
+            }
+            $string .= $this->css[$this->i];
+            $this->i++;
+        }
+    }
+}
