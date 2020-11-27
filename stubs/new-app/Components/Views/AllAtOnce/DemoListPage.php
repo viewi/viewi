900a6df364fa3997bb51b42380362b5563c4f85b
Ivan Voitovych <voitovych.ivan.v@gmail.com>
2020-11-27 12:12:34 +0200

    example get started app

--- stubs/new-app/Components/Views/AllAtOnce/DemoListPage.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Components\Views\AllAtOnce;
-
-use Viewi\BaseComponent;
-
-class DemoListPage extends BaseComponent
-{
-}
--- stubs/new-app/routes.php
+++ stubs/new-app/routes.php
@@ -1,18 +1,12 @@
 <?php
 
-use Components\Views\AllAtOnce\DemoListPage;
 use Components\Views\Home\HomePage;
 use Components\Views\NotFound\NotFoundPage;
 use Components\Views\Pages\CounterPage;
-use Components\Views\Pages\StatefulCounterPage;
-use Components\Views\Pages\StatefulTodoAppPage;
 use Components\Views\Pages\TodoAppPage;
 use Viewi\Routing\Route as ViewiRoute;
 
 ViewiRoute::get('/', HomePage::class);
 ViewiRoute::get('/counter', CounterPage::class);
 ViewiRoute::get('/todo', TodoAppPage::class);
-ViewiRoute::get('/stateful-counter', StatefulCounterPage::class);
-ViewiRoute::get('/stateful-todo', StatefulTodoAppPage::class);
-ViewiRoute::get('/all-at-once', DemoListPage::class);
 ViewiRoute::get('*', NotFoundPage::class);
