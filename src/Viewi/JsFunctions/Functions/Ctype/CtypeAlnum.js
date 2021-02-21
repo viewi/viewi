2a25697cc0cdee9a8e2061ed61aa8de59e1ac276
Ivan Voitovych <voitovych.ivan.v@gmail.com>
2021-02-21 16:32:39 +0200

    ctype js, htmlentities js

--- src/Viewi/JsFunctions/Functions/Ctype/CtypeAlnum.js
+++ src/Viewi/JsFunctions/Functions/Ctype/CtypeAlnum.js
@@ -1,12 +1,3 @@
-function ctype_alnum (text) { 
-  var setlocale = require('../strings/setlocale')
-  if (typeof text !== 'string') {
-    return false
-  }
-  setlocale('LC_ALL', 0)
-  var $global = (typeof window !== 'undefined' ? window : global)
-  $global.$locutus = $global.$locutus || {}
-  var $locutus = $global.$locutus
-  var p = $locutus.php
-  return text.search(p.locales[p.localeCategories.LC_CTYPE].LC_CTYPE.an) !== -1
+function ctype_alnum(text) {
+  return /^[\w\d]+$/g.test(text);
 }
--- src/Viewi/JsFunctions/Functions/Ctype/CtypeSpace.js
+++ src/Viewi/JsFunctions/Functions/Ctype/CtypeSpace.js
@@ -1,12 +1,3 @@
-function ctype_space (text) { 
-  var setlocale = require('../strings/setlocale')
-  if (typeof text !== 'string') {
-    return false
-  }
-  setlocale('LC_ALL', 0)
-  var $global = (typeof window !== 'undefined' ? window : global)
-  $global.$locutus = $global.$locutus || {}
-  var $locutus = $global.$locutus
-  var p = $locutus.php
-  return text.search(p.locales[p.localeCategories.LC_CTYPE].LC_CTYPE.sp) !== -1
+function ctype_space(text) {
+  return /^\s+$/g.test(text);
 }
