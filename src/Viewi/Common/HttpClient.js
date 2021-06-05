e26a5f1bebf99e12e1d4b30bb5963d2ab5df62c8
Ivan Voitovych <voitovych.ivan.v@gmail.com>
2021-06-05 16:29:13 +0300

    http client with interceptors (http handler concept)

--- src/Viewi/Common/HttpClient.js
+++ src/Viewi/Common/HttpClient.js
@@ -1,4 +1,5 @@
 var HttpClient = function () {
+    this.response = null;
     this.interceptors = [];
     this.options = {};
     var $this = this;
@@ -7,22 +8,105 @@ var HttpClient = function () {
         this.setOptions(options);
         var resolver = ajax.request(type, url, data, this.options);
         if (this.interceptors.length > 0) {
+            var nextHandler = null;
+            var handler = null;
+            var lastHandler = null;
+            var finalResolve = null;
+            var finalReject = null;
+            var response = {
+                success: false,
+                content: null,
+                canceled: false,
+                headers: {},
+                status: 0
+            };
+            var makeRequest = function (after) {
+                // console.log('==Request==');
+                lastHandler.after = after;
+                resolver.then(function (data) {
+                    response.success = true;
+                    response.content = data;
+                    after(lastHandler.next);
+                }, function (error) {
+                    response.content = error;
+                    after(lastHandler.next);
+                });
+            };
+            // var handlers = [];
             for (var i = this.interceptors.length - 1; i >= 0; i--) {
-                var httpMiddleWare = this.interceptors[i];
-                var nextAction = resolver.action;
-                resolver = (function (nextAction) {
-                    return new OnReady(function (onOk, onError) {
-                        httpMiddleWare[0][httpMiddleWare[1]]($this,
-                            // next
-                            function () {
-                                nextAction(onOk, onError);
-                            },
-                            // onError
-                            onError
-                        );
-                    })
-                })(nextAction);
+                var interceptor = this.interceptors[i];
+                var entryCall = interceptor[0][interceptor[1]];
+
+                handler = {
+                    response: response,
+                    handle: makeRequest,
+                    onHandle: entryCall,
+                    httpClient: $this,
+                    after: function () {
+                        // console.log('empty after');
+                    },
+                    next: function () {
+                        // console.log('next called', this);
+                        if (this.previousHandler) {
+                            this.previousHandler.after(this.previousHandler.next);
+                        } else {
+                            // this.after(function () {
+                            // console.log('--Resolving data--');
+                            if (response.success) {
+                                finalResolve(response.content);
+                            } else {
+                                finalReject(response.content);
+                            }
+                            // });
+                        }
+                        // call nextHandler.after(nextHandler.next);
+                    }
+                };
+                if (!lastHandler) {
+                    lastHandler = handler;
+                }
+                // handlers.unshift(handler);
+                handler.next = handler.next.bind(handler);
+                if (nextHandler) {
+                    handler.nextHandler = nextHandler;
+                    // (function (nextHandler, handler) {
+                    //     nextHandler.handle = handler.onHandle
+                    // })(nextHandler, handler);  
+                    nextHandler.previousHandler = handler;
+                    handler.handle = (function (nextHandler) {
+                        return function (after) {
+                            nextHandler.previousHandler.after = after;
+                            nextHandler.onHandle(nextHandler);
+                            // console.log('after', nextHandler);
+                        };
+                    })(nextHandler);
+                }
+                nextHandler = handler;
             }
+            // console.log(handlers);
+
+            return new OnReady(function (resolve, reject) {
+                finalResolve = resolve;
+                finalReject = reject;
+                handler.onHandle(handler);
+            });
+            // OLD
+            // for (var i = this.interceptors.length - 1; i >= 0; i--) {
+            //     var httpMiddleWare = this.interceptors[i];
+            //     var nextAction = resolver.action;
+            //     resolver = (function (nextAction) {
+            //         return new OnReady(function (onOk, onError) {
+            //             httpMiddleWare[0][httpMiddleWare[1]]($this,
+            //                 // next
+            //                 function () {
+            //                     nextAction(onOk, onError);
+            //                 },
+            //                 // onError
+            //                 onError
+            //             );
+            //         })
+            //     })(nextAction);
+            // }
         }
         return resolver;
     };
--- src/Viewi/Common/HttpClient.php
+++ src/Viewi/Common/HttpClient.php
@@ -10,29 +10,115 @@ class HttpClient
 {
     public array $interceptors = [];
     public array $options = [];
+    private $resolve;
+    private $reject;
 
     public function request($type, $url, $data = null, ?array $options = null)
     {
+
+        // intercept(HttpHandler $handler) 
+        // { 
+        //   // before
+        //   $handler->handle(($next) => { 
+        //                           // after
+        //                           $next(); 
+        //                           });
+        // }
+        $response = new HttpResponse();
+        $requestResolver = function (callable $resolve, callable $reject) use ($type, $url, $data, $response) {
+            $data = Route::handle($type, $url, $data);
+
+            if ($data instanceof Response) {
+                $response->content = $data->Content;
+                $response->headers = $data->Headers;
+                $response->status = $data->StatusCode;
+                if ($data->StatusCode >= 400 || $data->StatusCode < 200) {
+                    $response->success = false;
+                    $reject(new Exception("Error getting the response"));
+                }
+                $resolve($data->Content);
+            }
+            // echo " == request $type $url == \n<br>";
+            $response->status = 200;
+            $response->content = $data;
+            $resolve($data);
+        };
+
+        $count = count($this->interceptors);
+        if ($count > 0) {
+
+            $onHandle = function () use ($requestResolver) {
+                $requestResolver(function () {
+                }, function () {
+                });
+            };
+
+            $previousHandler = null;
+            for ($i = $count - 1; $i >= 0; $i--) {
+                $httpHandler = new HttpHandler();
+                $httpHandler->response = $response;
+                $httpHandler->top = $i === 0;
+                $httpHandler->previousHandler = $previousHandler;
+                $previousHandler = $httpHandler;
+                $httpHandler->httpClient = $this;
+
+                $httpHandler->onHandle = $onHandle;
+
+                $interceptor = $this->interceptors[$i][0];
+                $method = $this->interceptors[$i][1];
+
+                // action before
+                // $handler->handle(...)
+                // next handler or request => response
+                $onHandle = function () use ($httpHandler, $interceptor, $method) {
+                    $interceptor->$method($httpHandler);
+
+                    if ($httpHandler->after !== null && ($httpHandler->previousHandler === null || $httpHandler->previousHandler->continue)) {
+                        ($httpHandler->after)(function () use ($httpHandler) {
+                            $httpHandler->continue = true;
+                            if ($httpHandler->top) {
+                                // echo ' --RESOLVING TOP-- ';
+                                if ($httpHandler->response->success) {
+                                    ($this->resolve)($httpHandler->response->content);
+                                } else {
+                                    ($this->reject)($httpHandler->response->content);
+                                }
+                            }
+                        });
+                    }
+                };
+            }
+            return new PromiseResolver(function (callable $resolve, callable $reject) use ($onHandle) {
+                $this->resolve = $resolve;
+                $this->reject = $reject;
+                $onHandle();
+            });
+        }
+
+        $resolver = new PromiseResolver($requestResolver);
+
+        return $resolver;
+
         $onSuccess = function () use ($type, $url, $data) {
             $data = Route::handle($type, $url, $data);
             if ($data instanceof Response) {
-                if ($data->StatusCode >= 300 || $data->StatusCode <= 300) {
+                if ($data->StatusCode >= 400 || $data->StatusCode < 200) {
                     throw new Exception("Error getting the response");
                 }
                 return $data->Content;
             }
             return $data;
         };
-        $count = count($this->interceptors);
+
+
         if ($count > 0) {
             for ($i = $count - 1; $i >= 0; $i--) {
                 $httpMiddleware = $this->interceptors[$i][0];
                 $method = $this->interceptors[$i][1];
-                $httpMiddleware->$method($this, function () {
-                    // nothing
-                }, function () {
-                    // nothing
-                });
+                $httpHandler = new HttpHandler($onSuccess);
+                $httpHandler->httpClient = $this;
+                $httpMiddleware->$method($httpHandler);
+                // if called next - continue
             }
         }
 
