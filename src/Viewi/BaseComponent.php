4d61cd76256baac81a145a37515f88b3954b6157
Ivan Voitovych <col606v@gmail.com>
2022-01-16 14:28:44 +0200

    BaseComponent: container support

--- src/Viewi/BaseComponent.php
+++ src/Viewi/BaseComponent.php
@@ -2,10 +2,12 @@
 
 namespace Viewi;
 
+use Viewi\DI\IContainer;
+
 abstract class BaseComponent
 {
-    function __invoke($params)
+    function __invoke($params, ?IContainer $container = null)
     {
-        return App::run(get_class($this), $params);
+        return App::run(get_class($this), $params, $container);
     }
 }
