8684e64227373419dfbac0aa445c58ffefeebc28
Ivan Voitovych <col606v@gmail.com>
2022-01-22 17:09:14 +0200

    ViewiScripts: async

--- src/Viewi/Components/Assets/ViewiScripts.php
+++ src/Viewi/Components/Assets/ViewiScripts.php
@@ -4,14 +4,25 @@ namespace Viewi\Components\Assets;
 
 use Viewi\BaseComponent;
 use Viewi\Common\HttpClient;
+use Viewi\Components\Services\AsyncStateManager;
 
 class ViewiScripts extends BaseComponent
 {
     private HttpClient $http;
+    private AsyncStateManager $asyncStateManager;
+    public string $responses = '{}';
 
-    public function __construct(HttpClient $httpClient)
+    public function __construct(HttpClient $httpClient, AsyncStateManager $asyncStateManager)
     {
         $this->http = $httpClient;
+        $this->asyncStateManager = $asyncStateManager;
+        <<<javascript
+        return; // data scripts only for backend
+        javascript;
+        $subscription = $this->asyncStateManager->subscribe('httpReady');
+        $subscription->then(function () {
+            $this->responses = json_encode($this->http->getScopeResponses());
+        });
     }
 
     public function getDataScript()
@@ -19,7 +30,6 @@ class ViewiScripts extends BaseComponent
         <<<javascript
         return '<script>/** BLANK */</script>'; // data scripts only for backend
         javascript;
-        $responses = json_encode($this->http->getScopeResponses());
-        return "<script>window.viewiScopeData = $responses</script>";
+        return "<script>window.viewiScopeData = {$this->responses};</script>";
     }
 }
