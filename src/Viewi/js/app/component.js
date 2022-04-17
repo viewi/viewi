1ef1f76b73116c34b3088470c9f31bc18c3c41ad
Ivan Voitovych <col606v@gmail.com>
2022-04-17 19:28:34 +0300

    BaseComponent, emitEvent

---
+++ src/Viewi/js/app/component.js
@@ -0,0 +1,13 @@
+var BaseComponent = function () {
+    this.emitEvent = function (name) {
+        var event = arguments.length > 1 ? arguments[1] : null;
+        // console.log('event has been raised:', name, event, 'ON', this);
+        if (this.$_callbacks && name in this.$_callbacks) {
+            this.$_callbacks[name](event);
+        }
+    }
+}
+
+var $base = function (instance) {
+    BaseComponent.apply(instance);
+}
