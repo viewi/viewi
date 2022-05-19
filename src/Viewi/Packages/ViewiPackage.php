a225b44fe090d12e185636291fc8d30ddce8085b
Ivan Voitovych <col606v@gmail.com>
2022-05-19 16:13:00 +0300

    ViewiPackage

---
+++ src/Viewi/Packages/ViewiPackage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Viewi\Packages;
+
+use Viewi\PageEngine;
+
+abstract class ViewiPackage
+{
+    abstract function getComponentsPath(): ?string;
+
+    abstract function onBuild(PageEngine $pageEngine): void;
+}
