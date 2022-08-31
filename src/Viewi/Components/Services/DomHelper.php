f407e03439a19cbca1688a241fb2903a4448312d
Ivan Voitovych <col606v@gmail.com>
2022-08-31 13:30:08 +0300

    DomHelper: getWindow

--- src/Viewi/Components/Services/DomHelper.php
+++ src/Viewi/Components/Services/DomHelper.php
@@ -14,4 +14,13 @@ class DomHelper
         // nothing on server-side
         return null;
     }
+
+    public static function getWindow(): ?HtmlNode
+    {
+        <<<'javascript'
+        return window;
+        javascript;
+        // nothing on server-side
+        return null;
+    }
 }
