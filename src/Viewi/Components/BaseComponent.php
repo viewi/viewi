ad58eec2f1d69bc919a97749b6365b7db6a697aa
Ivan Voitovych <col606v@gmail.com>
2023-08-13 15:37:45 +0300

    BaseComponent

---
+++ src/Viewi/Components/BaseComponent.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Viewi\Components;
+
+abstract class BaseComponent
+{
+    public string $__id;
+    public array $_props = [];
+    /**
+     * 
+     * @var HtmlNode[]
+     */
+    public array $_refs = [];
+    // public ?HtmlNode $_element = null;
+    /**
+     * 
+     * @var array<string|int,bool>
+     */
+    public array $_slots = [];
+
+    function emitEvent(string $eventName, $event = null)
+    {
+        // nothing here, only client-side
+    }
+}
