47b3037e49452328142a7c920eee39db5b7d8485
Ivan Voitovych <col606v@gmail.com>
2023-08-25 12:37:12 +0300

    RenderFunction template

--- src/Viewi/Meta/RenderFunction.php
+++ src/Viewi/Meta/RenderFunction.php
@@ -1,15 +1,12 @@
 <?php
 
 function RenderFunction(
-    //Viewi\Engine 
-    $_engine,
-    //BaseComponent 
-    $_component,
+    Viewi\Engine $_engine,
+    BaseComponent $_component,
     array $_slots,
     array $_scope
 ) {
     $_content = '';
     //#content
-    // Viewi\Helpers::debug([__FUNCTION__, $_slots, $_scope]);
     return $_content;
 }
