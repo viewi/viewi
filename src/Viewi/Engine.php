4b98852c567b0b117dc3b59e5e6198d91a8afa56
Ivan Voitovych <col606v@gmail.com>
2023-08-25 12:37:51 +0300

    Engine

--- src/Viewi/Engine.php
+++ src/Viewi/Engine.php
@@ -2,9 +2,47 @@
 
 namespace Viewi;
 
+use Exception;
+
 class Engine
 {
-    public function __construct()
+    private bool $ready = false;
+    private array $meta = [];
+
+    public function __construct(private string $buildPath)
     {
+    }
+
+    public function render(string $component, array $params = [])
+    {
+        if (!$this->ready) {
+            $this->meta = require $this->buildPath . DIRECTORY_SEPARATOR . 'components.php';
+            $this->ready = true;
+        }
+        $component = strpos($component, '\\') !== false ?
+            substr(strrchr($component, "\\"), 1)
+            : $component;
+        return $this->renderComponent($component, $params, [], []);
+    }
+
+    public function renderSlot($component, $scope, $slotFunc, $parentSlots)
+    {
+        return $slotFunc($this, $component, $parentSlots, $scope);
+    }
+
+    public function renderComponent(string $component, $props, $slots, $scope)
+    {
+        if (
+            !isset($this->meta['components'][$component])
+            || !isset($this->meta['components'][$component]['Function'])
+        ) {
+            throw new Exception("Component '$component' not found.");
+        }
+        $componentMeta = $this->meta['components'][$component];
+        $fullClassName = $componentMeta['Namespace'] . '\\' . $componentMeta['Name'];
+        $classInstance = new $fullClassName();
+        include_once $this->buildPath . DIRECTORY_SEPARATOR . $componentMeta['Path'];
+        $renderFunc = $componentMeta['Function'];
+        return $renderFunc($this, $classInstance, $slots, $scope);
     }
 }
