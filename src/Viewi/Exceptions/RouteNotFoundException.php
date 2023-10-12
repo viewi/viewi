92681075751e0540dbfcd02174657af9892dc4b2
Ivan Voitovych <col606v@gmail.com>
2023-10-12 18:17:52 +0300

    RouteNotFoundException

---
+++ src/Viewi/Exceptions/RouteNotFoundException.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Viewi\Exceptions;
+
+use InvalidArgumentException;
+use Throwable;
+
+class RouteNotFoundException extends InvalidArgumentException
+{
+    protected array $routeData;
+
+    public function __construct(string $message = "", int $code = 0, Throwable $previous = null, array $routeData = [])
+    {
+        parent::__construct($message, $code, $previous);
+        $this->routeData = $routeData;
+    }
+
+    /**
+     * Missing route data
+     *
+     * @return array
+     */
+    public function getRouteData(): array
+    {
+        return $this->routeData;
+    }
+}
