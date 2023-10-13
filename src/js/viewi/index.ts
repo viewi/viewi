312f7306821e6dd6b5af3cea149e0d63502977f9
Ivan Voitovych <col606v@gmail.com>
2023-10-13 13:56:42 +0300

    viewi js: full page app

--- src/js/viewi/index.ts
+++ src/js/viewi/index.ts
@@ -1,7 +1,5 @@
-import { anchors } from "./core/anchor/anchors";
 import { ComponentsJson } from "./core/component/componentsJson";
 import { componentsMeta } from "./core/component/componentsMeta";
-import { renderComponent } from "./core/render/renderComponent";
 import { handleUrl } from "./core/router/handleUrl";
 
 const Viewi = () => ({
@@ -11,29 +9,6 @@ globalThis.Viewi = Viewi
 export { Viewi };
 
 console.log('Viewi entry');
-
-const counterTarget = document.getElementById('counter');
-
-export function renderApp(name: string) {
-    console.time('renderApp');
-    renderComponent(counterTarget!, name, undefined, {}, true, false);
-    // console.log(anchors);
-    // return;
-    for (let a in anchors) {
-        const anchor = anchors[a];
-        // clean up what's left
-        for (let i = anchor.target.childNodes.length - 1; i >= anchor.current + 1; i--) {
-            anchor.target.childNodes[i].remove();
-        }
-        // clean up not matched
-        for (let i = anchor.invalid.length - 1; i >= 0; i--) {
-            anchor.target.childNodes[anchor.invalid[i]].remove();
-        }
-    }
-    // console.timeEnd('renderApp');
-    // console.timeLog('renderApp');
-    console.timeEnd('renderApp');
-}
 
 // testing Counter
 (async () => {
