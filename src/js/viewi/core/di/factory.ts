f4d8cdc0ce26c481a4c3da9bfa8433f452d31629
Ivan Voitovych <col606v@gmail.com>
2023-10-20 10:44:15 +0300

    viewi js: custom factory, register

---
+++ src/js/viewi/core/di/factory.ts
@@ -0,0 +1,10 @@
+import { register } from "./register";
+
+type Constructor = new (...args: any[]) => any;
+
+export const factoryContainer: { [name: string]: <T>() => T } = {};
+
+export function factory<T extends Constructor>(name: string, implementation: T, factory: () => InstanceType<T>) {
+    register[name] = implementation;
+    factoryContainer[name] = factory;
+};
---
+++ src/js/viewi/core/di/register.ts
@@ -0,0 +1,1 @@
+export const register: { [name: string]: any } = {};
