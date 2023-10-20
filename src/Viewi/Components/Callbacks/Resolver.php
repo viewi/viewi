5df6e38ec9946b863f972d9615e15e1619b3029d
Ivan Voitovych <col606v@gmail.com>
2023-10-20 10:40:55 +0300

    Resolver

---
+++ src/Viewi/Components/Callbacks/Resolver.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Viewi\Components\Callbacks;
+
+use Exception;
+
+class Resolver
+{
+    /**
+     * 
+     * @var callable $onSuccess
+     */
+    protected $onSuccess;
+    /**
+     * 
+     * @var callable $onError
+     */
+    protected $onError = null;
+    /**
+     * 
+     * @var callable $onAlways
+     */
+    protected $onAlways;
+    protected $result = null;
+    protected $lastError = null;
+    /**
+     * 
+     * @param callable $action 
+     * @return void 
+     */
+    public function __construct(protected $action)
+    {
+    }
+
+    public function error(callable $onError)
+    {
+        $this->onError = $onError;
+    }
+
+    public function success(callable $onSuccess)
+    {
+        $this->onSuccess = $onSuccess;
+    }
+
+    public function always(callable $always)
+    {
+        $this->onAlways = $always;
+    }
+
+    public function then(callable $onSuccess, callable $onError = null, callable $always = null)
+    {
+        if ($onError !== null) {
+            $this->onError = $onError;
+        }
+        if ($always !== null) {
+            $this->onAlways = $always;
+        }
+        $throwError = false;
+        try {
+            $this->result = ($this->action)();
+            $onSuccess($this->result);
+        } catch (Exception $ex) {
+            $this->lastError = $ex;
+            if ($this->onError !== null) {
+                ($this->onError)($ex);
+            } else {
+                $throwError = true;
+            }
+        }
+        if ($this->onAlways != null) {
+            ($this->onAlways)();
+        }
+        if ($throwError) {
+            throw $this->lastError;
+        }
+    }
+}
