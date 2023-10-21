e0ed3a0cb589ebb773994276b658db76cafe3a74
Ivan Voitovych <col606v@gmail.com>
2023-10-21 18:36:51 +0300

    viewi js: http client - use scope responses

--- src/js/viewi/core/http/httpClient.ts
+++ src/js/viewi/core/http/httpClient.ts
@@ -7,6 +7,14 @@ class HttpClient {
     request(method: MethodType, url: string, body?: any, headers?: { [name: string]: string | string[] }) {
         const resolver = new Resolver(function (callback) {
             try {
+                const scopedResponseData: undefined | { [key: string]: any } = (<any>window).viewiScopeData;
+                if (typeof scopedResponseData !== 'undefined') {
+                    const requestKey = method.toLowerCase() + '_' + url + '_' + JSON.stringify(body);
+                    if (requestKey in scopedResponseData) {
+                        callback(scopedResponseData[requestKey]);
+                        return;
+                    }
+                }
                 request(function (response: Response) {
                     if (response.status === 0 || (response.status >= 200 && response.status < 400)) {
                         callback(response.data);
