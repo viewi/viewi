ae04d872c8e8bdf78bba489ea95facf45e39b0f6
Ivan Voitovych <col606v@gmail.com>
2023-11-03 17:22:53 +0200

    viewi js: http client

--- src/js/viewi/core/http/httpClient.ts
+++ src/js/viewi/core/http/httpClient.ts
@@ -5,13 +5,15 @@ import { request } from "./request";
 import { Response } from "./response";
 
 class HttpClient {
+    interceptors: string[] = [];
+
     request(method: MethodType, url: string, body?: any, headers?: { [name: string]: string | string[] }) {
         const resolver = new Resolver(function (callback) {
             try {
                 const state = getScopeState();
                 const requestKey = method.toLowerCase() + '_' + url + '_' + JSON.stringify(body);
                 if (requestKey in state.http) {
-                    callback(state.http[requestKey]);
+                    callback(JSON.parse(state.http[requestKey]));
                     delete state.http[requestKey];
                     return;
                 }
@@ -32,6 +34,12 @@ class HttpClient {
 
     get(url: string, headers?: { [name: string]: string | string[] }) {
         return this.request("get", url, null, headers);
+    }
+
+    withInterceptor(interceptor: string) {
+        const http = new HttpClient();
+        http.interceptors = [...this.interceptors, interceptor];
+        return http;
     }
 }
 
