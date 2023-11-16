e926481a7cc75efc431aef78a7286f92726a7fb0
Ivan Voitovych <col606v@gmail.com>
2023-11-16 14:33:22 +0200

    viewi js: set up base component

--- src/js/viewi/core/di/setUp.ts
+++ src/js/viewi/core/di/setUp.ts
@@ -1,8 +1,11 @@
+import { BaseComponent } from "../component/baseComponent";
 import { Process } from "../environment/process";
 import { HttpClient } from "../http/httpClient";
 import { factory } from "./factory";
+import { register } from "./register";
 
 export function setUp() {
+    register['BaseComponent'] = BaseComponent;
     factory('HttpClient', HttpClient, () => new HttpClient());
     factory('Process', Process, () => new Process());
 }
