f9ba4b9f3306ea735e4ba578b993007dde6711d5
Ivan Voitovych <col606v@gmail.com>
2023-11-17 16:30:48 +0200

    viewi js: Viewi: build, name

--- src/js/viewi/core/viewi.ts
+++ src/js/viewi/core/viewi.ts
@@ -5,5 +5,7 @@ declare global {
 export type Viewi = {
     register: { [name: string]: any },
     version: string,
+    build: string,
+    name: string,
     publish: (group: string, importComponents: {}) => void
 };
