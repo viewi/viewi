3322e7d7c8732f7346085a063a6a5276bbe17c65
Ivan Voitovych <col606v@gmail.com>
2023-11-17 16:30:29 +0200

    viewi js: resources placeholder

--- src/js/app/main/resources/index.js
+++ src/js/app/main/resources/index.js
@@ -1,5 +1,10 @@
 export const resources = {
     // placeholder
     componentsPath: '/components.json',    
-    publicPath: '/'
+    publicPath: '/',
+    name: 'default',
+    minify: true,
+    appendVersion: true,
+    build: '000',
+    version: 'v2.0.0'
 };
