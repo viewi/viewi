a8c42be92f314cb77720bd940ef5690291d231a1
Ivan Voitovych <col606v@gmail.com>
2023-11-18 13:19:22 +0200

    viewi js: combine support

--- src/js/app/main/components/index.js
+++ src/js/app/main/components/index.js
@@ -1,3 +1,5 @@
 export const components = {
     // placeholder
 };
+
+export const templates = "{}";
--- src/js/app/main/resources/index.js
+++ src/js/app/main/resources/index.js
@@ -4,6 +4,7 @@ export const resources = {
     publicPath: '/',
     name: 'default',
     minify: true,
+    combine: true,
     appendVersion: true,
     build: '000',
     version: 'v2.0.0'
