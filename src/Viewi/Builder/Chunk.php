9d52459a02fe332e73adfa3dc0c5cdbf99eede8d
Ivan Voitovych <col606v@gmail.com>
2023-11-18 13:18:09 +0200

    Chunk: public file names

--- src/Viewi/Builder/Chunk.php
+++ src/Viewi/Builder/Chunk.php
@@ -12,7 +12,10 @@ class Chunk
     public array $functions = [];
     public string $distFileName = '';
     public string $distFileMinName = '';
+    public string $publicFileName = '';
+    public string $publicFileMinName = '';
     public string $distFileJsonName = '';
+    public string $ppublicFileJsonName = '';
 
     public function __construct(public string $name, public string $jsComponentsPath, public string $jsFunctionsPath, public string $jsResourcesPath)
     {
