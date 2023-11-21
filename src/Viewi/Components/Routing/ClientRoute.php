54c18c7fe429b0804285069012dcd883653159c1
Ivan Voitovych <col606v@gmail.com>
2023-11-21 11:39:23 +0200

    ClientRoute

---
+++ src/Viewi/Components/Routing/ClientRoute.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Viewi\Components\Routing;
+
+use Viewi\Components\Environment\Platform;
+use Viewi\DI\Singleton;
+
+#[Singleton]
+class ClientRoute
+{
+    private array $config;
+    public function __construct(private Platform $platform)
+    {
+        $this->config = $platform->getConfig();
+    }
+
+    public function navigateBack()
+    {
+        $this->platform->navigateBack();
+    }
+
+    public function navigate(string $url)
+    {
+        $this->platform->redirect($url);
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->platform->getCurrentUrl();
+    }
+}
