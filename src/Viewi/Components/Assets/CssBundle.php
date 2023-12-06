525650a80228156db852fc3954d551fd3bfc20f9
Ivan Voitovych <col606v@gmail.com>
2023-12-06 14:19:00 +0200

    CssBundle

--- src/Viewi/Components/Assets/CssBundle.php
+++ src/Viewi/Components/Assets/CssBundle.php
@@ -2,12 +2,18 @@
 
 namespace Viewi\Components\Assets;
 
+use Exception;
 use Viewi\Components\Attributes\PostBuildAction;
 use Viewi\Components\BaseComponent;
+use Viewi\Components\Config\ConfigService;
 
 #[PostBuildAction(CssBundlePostBuildAction::class)]
 class CssBundle extends BaseComponent
 {
+    public function __construct(private ?ConfigService $config = null)
+    {
+    }
+
     public array $links = [];
     public bool $minify = false;
     public bool $combine = false;
@@ -16,10 +22,25 @@ class CssBundle extends BaseComponent
 
     public function getHtml(): string
     {
-        $cssHtml = '';
-        foreach ($this->links as $link) {
-            $cssHtml .= "<link rel=\"stylesheet\" href=\"/viewi-endoaid{$link}\">";
+        $baseUrl = $this->config->get('assetsUrl');
+        if ($this->combine) {
+            $cssBundleList = $this->config->get('cssBundle');
+            $version = $this->version();
+            if (!(isset($cssBundleList[$version]))) {
+                throw new Exception('Css bundle not found');
+            }
+            $cssFile = $baseUrl . $cssBundleList[$version];
+            return "<link rel=\"stylesheet\" href=\"{$cssFile}\">";
         }
-        return $cssHtml;
+    }
+
+    public function version(): string
+    {
+        $key = implode('|', $this->links);
+        $key .= $this->minify ? '1' : '0';
+        $key .= $this->inline ? '1' : '0';
+        $key .= $this->purge ? '1' : '0';
+        $key .= $this->combine ? '1' : '0';
+        return $key;
     }
 }
