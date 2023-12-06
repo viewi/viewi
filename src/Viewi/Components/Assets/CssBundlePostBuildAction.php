e8084a6c4030936359cf937ad6096949da604585
Ivan Voitovych <col606v@gmail.com>
2023-12-06 14:19:06 +0200

    CssBundlePostBuildAction

--- src/Viewi/Components/Assets/CssBundlePostBuildAction.php
+++ src/Viewi/Components/Assets/CssBundlePostBuildAction.php
@@ -3,14 +3,46 @@
 namespace Viewi\Components\Assets;
 
 use Viewi\Builder\Attributes\Skip;
+use Viewi\Builder\BuildAction\BuildActionItem;
+use Viewi\Builder\BuildAction\IPostBuildAction;
 use Viewi\Builder\Builder;
-use Viewi\Components\PostBuild\IPostBuildAction;
 
 #[Skip]
 class CssBundlePostBuildAction implements IPostBuildAction
 {
-    public function build(Builder $builder, array $props)
+    public function build(Builder $builder, array $props): ?BuildActionItem
     {
-        print_r($props);
+        $cssBundle = new CssBundle();
+        $cssBundle->links = $props['links'] ?? [];
+        $cssBundle->minify = $props['minify'] ?? false;
+        $cssBundle->combine = $props['combine'] ?? false;
+        $cssBundle->inline = $props['inline'] ?? false;
+        $cssBundle->purge = $props['purge'] ?? false;
+        $version = $cssBundle->version();
+        $output = $cssBundle->combine && count($cssBundle->links) > 1
+            ? '/' . crc32($version) . '.css'
+            : $cssBundle->links[0];
+        if ($cssBundle->minify || $cssBundle->purge) {
+            $pathInfo = pathinfo($output, PATHINFO_ALL);
+            $output = $pathInfo['dirname'] . $pathInfo['basename'];
+            if ($cssBundle->purge) {
+                $output .= '.pg';
+            }
+            if ($cssBundle->minify) {
+                $output .= '.min';
+            }
+            $output .= '.css';
+        }
+        return new BuildActionItem('css', [
+            'links' => $cssBundle->links,
+            'minify' => $cssBundle->minify,
+            'combine' => $cssBundle->combine,
+            'inline' => $cssBundle->inline,
+            'purge' => $cssBundle->purge,
+            'version' => $version,
+            'output' => $output
+        ], [
+            'cssBundle' => [$version => $output]
+        ]);
     }
 }
