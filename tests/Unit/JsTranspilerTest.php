a9189988ce21500b29c85c1889c5f7d2a17ee30f
Ivan Voitovych <col606v@gmail.com>
2023-12-08 15:43:11 +0200

    JsTranspilerTest

--- tests/Unit/JsTranspilerTest.php
+++ tests/Unit/JsTranspilerTest.php
@@ -10,12 +10,16 @@ use Viewi\JsTranspile\JsTranspiler;
 class JsTranspilerTest extends \Codeception\Test\Unit
 {
     protected UnitTester $tester;
+    protected JsTranspiler $transpiler;
 
+    protected function _before()
+    {
+        $this->transpiler = new JsTranspiler();
+    }
     // tests
     public function testBasic()
     {
-        $transpiler = new JsTranspiler();
-        $jsCode = $transpiler->convert('<?php $a = "Hello World";');
+        $jsCode = $this->transpiler->convert('<?php $a = "Hello World";');
         $this->assertEquals('var a = "Hello World";', trim($jsCode));
     }
 }
