f32f8733d973bfb157949ebc7feba559aa3d3057
Ivan Voitovych <col606v@gmail.com>
2024-01-30 12:58:08 +0200

    viewi js: base component provide and inject

--- src/js/viewi/core/component/baseComponent.ts
+++ src/js/viewi/core/component/baseComponent.ts
@@ -11,10 +11,24 @@ export abstract class BaseComponent<T> {
     $$r: { [key: string]: { [key: string]: [Function, any[]] } } = {}; // reactivity callbacks
     $$p: [trackerId: string, activated: ReactiveProxy][] = []; // shared reactivity track ids
     $: T;
+    _provides: Object;
+    _parent: null | BaseComponent<any> = null;
     _name: string = 'BaseComponent';
+
     emitEvent(name: string, event?: any) {
         if (name in this.$_callbacks) {
             this.$_callbacks[name](event);
         }
+    }
+
+    provide(key: string, value: any): void {
+        if (this._provides === this._parent?._provides) {
+            this._provides = Object.create(this._provides);
+        }
+        this._provides[key] = value;
+    }
+
+    inject(key: string): any {
+        return this._provides[key] || null;
     }
 }
