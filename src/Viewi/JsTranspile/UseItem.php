277fd17de541d95fcd11074b2b7593255942315f
Ivan Voitovych <col606v@gmail.com>
2024-02-10 10:24:13 +0200

    UseItem: System

--- src/Viewi/JsTranspile/UseItem.php
+++ src/Viewi/JsTranspile/UseItem.php
@@ -6,6 +6,7 @@ class UseItem
 {
     const Class_ = 'CL';
     const Function = 'F';
+    const System = 'S';
     public bool $Skip = false;
 
     public function __construct(public array $Parts, public string $Type)
