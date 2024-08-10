5c2deda5ce9807fc32174896fafb9094adf03438
Ivan Voitovych <col606v@gmail.com>
2024-08-10 16:54:32 +0300

    array_reduce

--- src/Viewi/PhpJsFunctions/Array/ArrayReduce.js
+++ src/Viewi/PhpJsFunctions/Array/ArrayReduce.js
@@ -1,25 +1,13 @@
-function array_reduce (aInput, callback) { // eslint-disable-line camelcase
-  //  discuss at: https://locutus.io/php/array_reduce/
-  // original by: Alfonso Jimenez (https://www.alfonsojimenez.com)
-  //      note 1: Takes a function as an argument, not a function's name
-  //   example 1: array_reduce([1, 2, 3, 4, 5], function (v, w){v += w;return v;})
-  //   returns 1: 15
-
-  const lon = aInput.length
-  let res = 0
-  let i = 0
-  let tmp = []
-
-  for (i = 0; i < lon; i += 2) {
-    tmp[0] = aInput[i]
-    if (aInput[(i + 1)]) {
-      tmp[1] = aInput[(i + 1)]
-    } else {
-      tmp[1] = 0
+function array_reduce(aInput, callback, initial) { // eslint-disable-line camelcase
+  let result = initial === undefined ? 0 : initial;
+  if (Array.isArray(aInput)) {
+    for (i = 0; i < aInput.length; i++) {
+      result = callback.apply(null, [result, aInput[i]]);
     }
-    res += callback.apply(null, tmp)
-    tmp = []
+  } else {
+    for (let k in aInput) {
+      result = callback.apply(null, [result, aInput[k]]);
+    }
   }
-
-  return res
+  return result;
 }
