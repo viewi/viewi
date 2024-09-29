dbdc417b4857e064840fdafeafa00077a511cbdb
Ivan Voitovych <col606v@gmail.com>
2024-09-29 18:59:07 +0300

    CustomJs

--- src/Viewi/Builder/Attributes/CustomJs.php
+++ src/Viewi/Builder/Attributes/CustomJs.php
@@ -5,10 +5,15 @@ namespace Viewi\Builder\Attributes;
 use Attribute;
 
 /**
- * Class marked with this attribute will not be included in JS bundle
+ * Class marked with this attribute will have custom JavaScript
  * @package Viewi\Builder\Attributes
  */
 #[Attribute(Attribute::TARGET_CLASS)]
 class CustomJs
 {
+    /**
+     * @param bool $export True (default) if you want to include the code automatically 
+     * @return void 
+     */
+    public function __construct(public bool $export = true) {}
 }
