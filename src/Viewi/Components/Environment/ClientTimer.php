a660d8981a26dd9d6e4a07f4bb21bb2ad8e904dd
Ivan Voitovych <col606v@gmail.com>
2024-10-16 06:33:48 +0300

    ClientTimer

---
+++ src/Viewi/Components/Environment/ClientTimer.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Viewi\Components\Environment;
+
+use Viewi\DI\Singleton;
+
+#[Singleton]
+class ClientTimer
+{
+    public static function setTimeoutStatic(callable $action, int $milliseconds): int
+    {
+        <<<'javascript'
+        return window.setTimeout(action, milliseconds);
+        javascript;
+        // nothing on server-side
+        return 0;
+    }
+
+    public function setTimeout(callable $action, int $milliseconds): int
+    {
+        return self::setTimeoutStatic($action, $milliseconds);
+    }
+
+    public static function clearTimeoutStatic(int $timerId)
+    {
+        <<<'javascript'
+        window.clearTimeout(timerId);
+        return;
+        javascript;
+        // nothing on server-side 
+    }
+
+    public function clearTimeout(int $timerId)
+    {
+        self::clearTimeoutStatic($timerId);
+    }
+
+    public static function setIntervalStatic(callable $action, int $milliseconds): int
+    {
+        <<<'javascript'
+        return window.setInterval(action, milliseconds);
+        javascript;
+        // nothing on server-side
+        return 0;
+    }
+
+    public function setInterval(callable $action, int $milliseconds): int
+    {
+        return self::setIntervalStatic($action, $milliseconds);
+    }
+
+    public static function clearIntervalStatic(int $timerId)
+    {
+        <<<'javascript'
+        window.clearInterval(timerId);
+        return;
+        javascript;
+        // nothing on server-side
+    }
+
+    public function clearInterval(int $timerId)
+    {
+        self::clearIntervalStatic($timerId);
+    }
+}
