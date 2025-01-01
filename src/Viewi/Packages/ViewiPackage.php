7e4544fad046c776c3b08ae89ece01704f07651a
Ivan Voitovych <col606v@gmail.com>
2025-01-01 17:29:24 +0200

    ViewiPackage: assetsPath, getDependencies

--- src/Viewi/Packages/ViewiPackage.php
+++ src/Viewi/Packages/ViewiPackage.php
@@ -8,4 +8,10 @@ abstract class ViewiPackage
     abstract static function jsDir(): ?string;
     abstract static function jsModulePackagePath(): ?string;
     abstract static function name(): string;
+    abstract static function assetsPath(): ?string;
+    /**
+     * 
+     * @return ViewiPackage[] 
+     */
+    abstract static function getDependencies(): array;
 }
